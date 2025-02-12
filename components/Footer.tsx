1e03634809e14bda15f0301f4e64e4087b493cc1
Leo <jhonleosolahilario@gmail.com>
2025-02-12 15:06:11 +0800

    update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,7 +4,7 @@ const Footer: React.FC = () => {
         <div className="flex flex-1 items-end justify-center w-full font-fira text-center p-5 mt-5">
             <div className="text-light-text-primary dark:text-dark-text-primary text-xs flex space-x-2">
                 <img src="/icons/logo.svg" className="w-3" />
-                <span>{"ryan schachte -"}</span>
+                <span>{"CavCount Blog -"}</span>
                 <span>{new Date().getFullYear()}</span>
             </div>
         </div>
